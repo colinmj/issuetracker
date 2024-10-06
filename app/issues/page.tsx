1a53533d3d69131e6fbf02ff65e5277bd4b35232
Colin Matson <cmatsonjones@gmail.com>
2024-10-06 09:21:41 -0700

    Built new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 const Issues = () => {
-  return <div>Issues</div>;
+  return (
+    <Button>
+      <Link href="issues/new">New Issue</Link>
+    </Button>
+  );
 };
 
 export default Issues;
